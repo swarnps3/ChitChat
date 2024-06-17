f9c55f09a80ada50d7a8de1c0d5ea13e5a28027d
swarnps3 <swarnps3>
2024-06-17 15:31:39 +0530

    Started frontend

--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,27 @@
-const express=require('express');
-const dotenv=require('dotenv');
-const {chats}=require("./dummy/data");
+const express = require('express');
+const dotenv = require('dotenv');
+const { chats } = require("./dummy/data");
 
-const app=express();
+const app = express();
 dotenv.config();
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('API is running on this page with server');
 })
 
-app.get('/api/chats', (req, res)=>{
+app.get('/api/chats', (req, res) => {
     res.send(chats);
 })
 
-app.get('/api/chats/:id', (req, res)=>{
+app.get('/api/chats/:id', (req, res) => {
     console.log(req.params.id);
-    const singleChat=chats.find((c)=>c._id===req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
     res.send(singleChat);
 });
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server started at port ${PORT} successfully`);
 });
